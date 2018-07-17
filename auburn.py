533f1d0fda363267cbe16db2004a54132641623b
Jared Kick <jaredkick@gmail.com>
2018-07-17 17:21:49 +0000

    Update auburn.py

---
+++ auburn.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# A simple script that takes audiobook files, renames them, and tags them properly
+
+#import
+
+__author__ = "Jared Kick"
+__copyright__ = ""
+__credits__ = ["Jared Kick"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Jared Kick"
+__email__ = "jaredkick@gmail.com"
+__status__ = "Prototype"
+
+# Iterate through folder
+
+# Cleanse filename
+
+    # If file contains "excerpt", delete it
+
+    # Remove major words
+
+    # Remove special characters
+
+    # Handle chapters/parts
+    
+# Search Google Books API
+
+# Compare titles
+
+# Rename filename
+
+# Add tags
+
+# Move to audiobooks folder
