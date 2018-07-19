466bfce7a7ccd916e2a55338673b81eab2539509
Jared Kick <jaredkick@gmail.com>
2018-07-19 02:10:08 +0000

    Update auburn.py

--- auburn.py
+++ auburn.py
@@ -8,6 +8,8 @@ from google_images_download import google_images_download
 from apiclient.discovery import build
 # For manipulating API responses
 import json
+# For various system commands like moving and renaming files
+import os
 
 __author__ = "Jared Kick"
 __copyright__ = ""
@@ -17,6 +19,9 @@ __version__ = "0.0.1"
 __maintainer__ = "Jared Kick"
 __email__ = "jaredkick@gmail.com"
 __status__ = "Prototype"
+
+
+AUDIOBOOK_DIR = "/home/jared/Software/Development/Auburn/AUDIOBOOK_DIR/"
 
 # Iterate through folder
 
@@ -35,15 +40,15 @@ __status__ = "Prototype"
 # https://developers.google.com/api-client-library/python/start/get_started#building-and-calling-a-service
 # https://developers.google.com/books/docs/v1/getting_started
 # Build service
-service = build('books', 'v1')
+#service = build('books', 'v1')
 # Setup API collection
-collection = service.volume()
+#collection = service.volume()
 # Make request
-request = collection.list()
+#request = collection.list()
 # Send request
-response = request.execute()
+#response = request.execute()
 # Print response (temporary)
-print json.dumps(response, sort_keys=True, indent=4)
+#print json.dumps(response, sort_keys=True, indent=4)
 
 # Compare titles
 
@@ -51,13 +56,33 @@ print json.dumps(response, sort_keys=True, indent=4)
 
 # Add tags
 
+
+# Temporary inputs for testing
+author = input("Enter author: ")
+title = input("Enter title: ")
+
 # Get audiobook cover art
 # https://github.com/hardikvasa/google-images-download
 response = google_images_download.googleimagesdownload()
 # Set search keywords to "author book title - audiobook" and limit to one result
-arguments = {"keywords": author + " \"" + title + "\"" + " audiobook", "limit":1, "aspect_ratio":"square"}
+search_term = "\"" + author + " " + title + " audiobook\""
 
+# Set Google search arguments
+arguments = {"keywords":search_term, "limit":1, "aspect_ratio":"square", "no_directory":True}
+# Download album art
 paths = response.download(arguments)
-print(paths)
+
+# Keep hold of location and name for later
+image_location = paths[search_term][0]
+filename = os.path.basename(image_location)
+print(filename)
+
+# Get filename extension
+filename, file_extension = os.path.splitext(image_location)
+
+print(filename)
+print(file_extension)
+# Rename file to "folder" with the original extension
+os.rename(filename + file_extension, AUDIOBOOK_DIR + "folder" + file_extension)
 
 # Move to audiobooks folder
